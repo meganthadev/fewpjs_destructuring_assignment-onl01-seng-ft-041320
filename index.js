489fa7667ae2a8dbf490712cedb5dbd297e38394
Megan Horsfield <meganthadev@gmail.com>
2020-08-05 03:47:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ const [ , , , , ,indg, ] = colors
 const {name, color, song, job, partner} = muppet
 
 const {song_2, song_4} = k_muppet.k_album.the_muppet_movie;
-const {job, partner} = k_muppet
+const {, k_job, k_partner} = k_muppet
 
 
 // 7. Use destructuring to assign all appropriate variables using the keys as the variable names
