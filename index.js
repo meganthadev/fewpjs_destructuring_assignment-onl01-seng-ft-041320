664c67744588c307feed5b8ad567b660b6eda04e
Megan Horsfield <meganthadev@gmail.com>
2020-08-05 03:43:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -47,6 +47,8 @@ const [red, orange, yellow, green, blue, indigo, violet] = colors
 
 const [r, o, y, g, b, ,v] = colors
 
+const [indg] = colors
+
 // 4. Use destructuring to assign appropriate variables using the color names.
 
 // 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
